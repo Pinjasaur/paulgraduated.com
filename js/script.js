1d3c859dd9f7953d08f18bbc474c2ab845634a05
Paul Esch-Laurent <herppfel@gmail.com>
2019-06-17 13:47:40 -0400

    Polishes

--- js/script.js
+++ js/script.js
@@ -1,1 +1,13 @@
 console.log("Hi there.")
+
+document.getElementById("grayscale").addEventListener("click", () => {
+  for (const $img of Array.from(document.querySelectorAll("img"))) {
+    $img.classList.toggle("grayscale")
+  }
+})
+
+for (const $img of Array.from(document.querySelectorAll("img"))) {
+  $img.addEventListener("click", (e) => {
+    e.target.classList.toggle("grayscale")
+  })
+}
